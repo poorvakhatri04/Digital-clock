70452ab1ee1a5e05f28deecf518fc9ae7a6631fe
Poorva khatri <poorvakhatri04@gmail.com>
2024-08-05 14:05:41 +0530

    Changes in javascript

---
+++ script.js
@@ -0,0 +1,17 @@
+let time=document.getElementById("time");
+let timeformat=document.getElementById("timeformat");
+document.addEventListener("DOMContentLoaded",()=>{
+    setInterval(showTime,1000);
+});
+const showTime=()=>{
+    let date=new Date();
+    let hr=date.getHours();
+    let min=date.getMinutes();
+    let secs=date.getSeconds();
+    hr=hr<10?`0${hr}`:hr;
+    min=min<10?`0${min}`:min;
+    secs=secs<10?`0${secs}`:secs;
+
+    time.innerHTML=`${hr} : ${min} : ${secs}`;
+    //console.log(hr+":"+min+":"+secs)
+}
